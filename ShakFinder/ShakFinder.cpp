087fd82d9dd04a0d84baec8c98c2cef4dbcf2106
shakkar23 <shakkar23@gmail.com>
2024-05-06 19:14:21 -0700

    add broken solution grabbing

--- ShakFinder/ShakFinder.cpp
+++ ShakFinder/ShakFinder.cpp
@@ -1,39 +1,49 @@
 ﻿#include "ShakFinder.h"
-#include "Engine/Parser.hpp"
-#include "Engine/Solver.hpp"
 
 #include <chrono>
 
-int main(int argc, char* argv[])
-{
-	std::span<char*> args(argv, argc);
+#include "Engine/Parser.hpp"
+#include "Engine/Solver.hpp"
 
-	std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-	auto queues = Parser::parse(Parser::preprocess("[SZ][JL][TOI]SZTLJOL"));
-	
-	std::cout << "generated queues!" << std::endl;
+int main(int argc, char* argv[]) {
+    std::span<char*> args(argv, argc);
 
-	for (int i = 0; i < queues.size(); i++)
-	{
-		bool pc = Solver::can_pc(Board(), queues[i]);
+    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+    auto queues = Parser::parse(Parser::preprocess("JTSLOZISLO"));
 
-		if(!pc)
-		{
-			std::cout << "we could not solve the pc for: " << i + 1 << std::endl;
-			std::cout << "the queue is: ";
-			for (PieceType& piece : queues[i])
-			{
-				std::cout << Parser::getChar(piece);
-			}
-			std::cout << std::endl << std::endl;
-		}
-		else 
-			std::cout << "solved a thing!" << std::endl;
-	}
+    std::cout << "generated queues!" << std::endl;
 
-	std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+    for (int i = 0; i < queues.size(); i++) {
+        auto pcs = Solver::solve_pcs(Board(), queues[i]);
 
-	std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[mili seconds]" << std::endl;
+        if (pcs.size() == 0) {
+            std::cout << "we could not solve the pc for: " << i + 1 << std::endl;
+            std::cout << "the queue is: ";
+            for (PieceType& piece : queues[i]) {
+                std::cout << Parser::getChar(piece);
+            }
+            std::cout << std::endl
+                      << std::endl;
+        } else {
+            std::cout << "solved a thing!" << std::endl;
+            std::cout << "the amount of PCs found is " << pcs.size() << std::endl;
+            for (const auto& path : pcs) {
+                if(path.size() == 10){
+                std::cout << "the path is " << path.size() << " long: ";
 
-	return 0;
+
+                for (const auto& piece : path) {
+                    std::cout << Parser::getChar(piece.type);
+                }
+                std::cout << std::endl;
+                }
+            }
+        }
+        std::cout << "number of pcs: " << pcs.size() << std::endl;
+    }
+
+    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+
+    std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[mili seconds]" << std::endl;
+    return 0;
 }
