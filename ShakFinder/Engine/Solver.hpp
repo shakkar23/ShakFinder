833726bc61ddc59cac8ec34fc0f0ee814d0e2ef5
shakkar23 <shakkar23@gmail.com>
2024-05-06 19:19:49 -0700

    update Solver.hpp

--- ShakFinder/Engine/Solver.hpp
+++ ShakFinder/Engine/Solver.hpp
@@ -1,15 +1,15 @@
 #pragma once
 
-#include "Piece.hpp"
-#include "Board.hpp"
+#include <vector>
 
+#include "Board.hpp"
+#include "Piece.hpp"
 
 namespace Solver {
 
-	// returns whether or not a pc is possible
-	bool can_pc(const Board& board, const Queue& queue);
+// returns whether or not a pc is possible
+bool can_pc(const Board& board, const Queue& queue);
 
-
-	// returns the Moves for every PC possible
-	std::vector<Piece> solve_pcs(const Board& board, const Queue& queue);
+// returns the Moves for every PC possible
+std::vector<std::vector<Piece>> solve_pcs(const Board& board, const Queue& queue);
 };
